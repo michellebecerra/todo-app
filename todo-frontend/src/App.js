3e004204953811ac5b3966e444b779b9639195f1
Michelle Becerra <michel8@vt.edu>
2021-07-14 16:41:04 -0400

    adding front end.

---
+++ todo-frontend/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+// Hooks ^ , componentDidMount
+function App() {
+  return (
+    <div>
+      <List />
+    </div>
+  );
+}
+
+const List = (props) => {
+  const [ todos, setTodos ] = useState([]);
+  const [ text, setText ] = useState([]);
+
+  // Ajax
+  const fetchTodos = async () => {
+    const res = await fetch('http://localhost:3001/todos');
+    //console.log(await res.json());
+    setTodos(await res.json());
+  };
+
+  // const addTodo = async () => {
+  //   const res = await fetch('http://localhost:3001/todos', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ desc: text, done: false })
+  //   });
+
+  //   fetchTodos();
+  //   setText('');
+  // };
+
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const items = todos.map(todo => <Item todo={todo} key={todo._id} fetchTodos={fetchTodos} />);
+
+  return (<div >
+{items}
+</div>);
+  
+  
+};
+
+const Item = (props) => {
+  const { done, desc, _id } = props.todo;
+  return <div>{desc}</div>;
+};
+
+
+
+export default App;
